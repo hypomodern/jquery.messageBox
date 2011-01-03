2a187602676a961e07f5ceb7eaf48e67517f63cc
Matt Wilson <mhw@hypomodern.com>
2011-01-03 09:42:49 -0500

    indentation.

--- example/jquery.closure.js
+++ example/jquery.closure.js
@@ -1,11 +1,11 @@
 jQuery.callback = function(o, m){
- return function(){
-  return m.apply(o, arguments);
- };
+  return function(){
+    return m.apply(o, arguments);
+  };
 };
 jQuery.closeArgs = function(o, m){
- var args = Array.prototype.slice.call(arguments, 2);
- return function(){
-  m.apply(o, args);
- };
+  var args = Array.prototype.slice.call(arguments, 2);
+  return function(){
+    m.apply(o, args);
+  };
 };
